e35f08915865b3c0b87253635434f32212673c8c
Sup3rgnu <Sup3rgnu@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-02 14:26:32 +0000

    board 2d array

--- src/mapping/Map.java
+++ src/mapping/Map.java
@@ -2,4 +2,17 @@ package mapping;
 
 public class Map {
 
+	final static int FREE = 0;  // empty square
+	final static int STONE = 1;  // square with a stone in it
+	final static int PLAYER = 2;   // square where a player is
+
+	int[][] board = new int[24][24]; {
+		
+		// Fill out the whole board with free squares
+		
+		for (int i=0; i < 24; i++)
+			   for (int j=0; j < 24; j++)
+			      board[i][j] = FREE;
+
+	}
 }
