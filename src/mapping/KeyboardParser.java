4341be2536096179b20f30d6532d23eefdfa5cec
Sup3rgnu <Sup3rgnu@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-02 21:19:51 +0000

    parser updates

--- src/mapping/KeyboardParser.java
+++ src/mapping/KeyboardParser.java
@@ -6,6 +6,18 @@ public class KeyboardParser {
 	public void netParse(char c) {
 		if (c == 'a') {
 			// goLeft
+		}else if (c == 'b'){
+			//goRight
+		}else if (c == 'c'){
+			//goUp
+		}else if (c == 'd'){
+			//goDown
+		}else if (c == 'e'){
+			//dropBomb
+		}else if (c == 'f'){
+			//esc
+		}else if (c == 'g'){
+			//something
 		}
 	}
 }
