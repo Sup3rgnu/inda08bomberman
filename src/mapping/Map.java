9559f2a2425215d0005d9f6bcb6e02f8148612db
Sup3rgnu <Sup3rgnu@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-02 15:10:46 +0000

    small update

--- src/mapping/Map.java
+++ src/mapping/Map.java
@@ -2,16 +2,21 @@ package mapping;
 
 public class Map {
 
-	final static int FREE = 0;  // empty square
-	final static int STONE = 1;  // square with a stone in it
-	final static int PLAYER = 2;   // square where a player is
+	final static int FREE = 0;  // Empty square
+	final static int STONE = 1;  // Square with a stone in it
+	final static int UNBREAKABLE = 2; // Unbreakable path, a player can't stand here
+	final static int PLAYER = 3;   // Square where a player is
+	
+	final static int HEIGHT = 24; // Height of the board
+	final static int WIDTH = 24; // Width of the board
 
-	int[][] board = new int[24][24]; {
+	int[][] board = new int[HEIGHT][WIDTH]; {
 		
 		// Fill out the whole board with free squares
+		// Assumes that everything outside the board is UNBREAKABLE
 		
-		for (int i=0; i < 24; i++)
-			   for (int j=0; j < 24; j++)
+		for (int i=0; i < HEIGHT ; i++)
+			   for (int j=0; j < WIDTH; j++)
 			      board[i][j] = FREE;
 
 	}
