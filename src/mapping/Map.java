2d7145b70fc7692b71c34945ada213a61003148c
Sup3rgnu <Sup3rgnu@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-03 15:20:12 +0000

    starting board positions, filled 2D array

--- src/mapping/Map.java
+++ src/mapping/Map.java
@@ -7,6 +7,8 @@ public class Map {
 	final static int UNBREAKABLE = 2; // Unbreakable path, a player can't stand here
 	final static int PLAYER1 = 3;   // Square where player 1 is
 	final static int PLAYER2 = 4;   // Square where player 2 is
+	final static int BOMB1 = -1; // Square with player1 has placed a bomb
+	final static int BOMB2 = -2; // Square with player2 has placed a bomb
 	
 	final static int HEIGHT = 24; // Height of the board
 	final static int WIDTH = 24; // Width of the board
@@ -14,11 +16,32 @@ public class Map {
 	int[][] board = new int[HEIGHT][WIDTH]; {
 		
 		// Fills out the whole board with free squares
-		// Assumes that everything outside the board is UNBREAKABLE
 		
 		for (int i=0; i < HEIGHT ; i++)
 			   for (int j=0; j < WIDTH; j++)
 			      board[i][j] = FREE;
 
+		// Outer boarder for the board with unbreakable pieces 
+		
+		for (int i=0; i <= 0; i++)
+			for (int j=0; j < WIDTH; j++)
+				board[i][j] = UNBREAKABLE;
+
+		for (int i=HEIGHT; i <= HEIGHT; i++)
+			for (int j=0; j < WIDTH; j++)
+				board[i][j] = UNBREAKABLE;
+
+		for (int i=0; i < HEIGHT; i++)
+			for (int j=0; j <= 0; j++)
+				board[i][j] = UNBREAKABLE;
+
+		for (int i=0; i < HEIGHT; i++)
+			for (int j=WIDTH; j <= WIDTH; j++)
+				board[i][j] = UNBREAKABLE;
+		
+		// Players starting positions, diagonally across from each other
+		
+		board[1][1] = PLAYER1;
+		board[HEIGHT-1][WIDTH-1] = PLAYER2;
 	}
 }
