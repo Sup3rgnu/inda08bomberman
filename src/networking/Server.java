a525d84052d6aff81d7d190d5be8772b2771eaa4
caj.hofberg <caj.hofberg@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-03 12:16:38 +0000

    Better popup usage

--- src/networking/Server.java
+++ src/networking/Server.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import drawing.DrawWindow;
+
 /**
  * A Server connection.
  * @author caj.hofberg
@@ -20,12 +22,16 @@ abstract public class Server {
 			drawing.Popup.popupMessage("Trying to start server");
 			ServerSocket serverSocket = new ServerSocket(Network.GAMEPORT);
 			
+			drawing.Popup.popupMessage("Server started. Opponents may now join");
+			DrawWindow.DrawWindowmain(); // Removes white traces after popup
 			Socket server = serverSocket.accept();
 			drawing.Popup.popupMessage("Connection accepted");
 			Network.handleTraffic(server);
 		}
 		catch (IOException e) {
            System.err.println(e);
+           drawing.Popup.popupMessage("Failed. See System.err");
+           //TODO: popup should return the exception message
         }
 	}
 }
