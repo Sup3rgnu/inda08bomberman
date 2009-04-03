74dcdd3057b26208bc3b0325858dbfe903385ead
Sup3rgnu <Sup3rgnu@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-03 13:26:19 +0000

    locale parse

--- src/mapping/KeyboardParser.java
+++ src/mapping/KeyboardParser.java
@@ -8,23 +8,42 @@ public class KeyboardParser {
 	//public static final int VK_UP = 38;
 	//public static final int VK_DOWN = 40;
 	//public static final int SPACE = 32;
-	//public static final int VK_ESCAPE = 27;
+	//public static final int ESCAPE = 27;
 	
 	public void netParse(char c) {
-		if (c == '%') {
+		if (c == (char) 37) {
 			// goLeft
-		}else if (c == 'ã'){
+		}else if (c == (char) 227){
 			//goRight
-		}else if (c == '&'){
+		}else if (c == (char) 38){
 			//goUp
-		}else if (c == '('){
+		}else if (c == (char) 40){
 			//goDown
-		}else if (c == ' '){
+		}else if (c == (char) 32){
 			//dropBomb
-		}else if (c == 'f'){
+		}else if (c == (char) 27){
 			//esc
-		}else if (c == 'g'){
-			//something
+		}else{
+			//doNothing
 		}
 	}
+	
+	public void localParse(char c) {
+		if (c == (char) 37) {
+			// goLeft
+		}else if (c == (char) 227){
+			//goRight
+		}else if (c == (char) 38){
+			//goUp
+		}else if (c == (char) 40){
+			//goDown
+		}else if (c == (char) 32){
+			//dropBomb
+		}else if (c == (char) 27){
+			//esc
+		}else{
+			//doNothing
+		}
+	}
+	
 }
