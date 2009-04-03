2e575fd94076a2047dd116562be7a8910770824a
caj.hofberg <caj.hofberg@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-03 13:39:28 +0000

    Removed hard-coded ints

--- src/mapping/KeyboardParser.java
+++ src/mapping/KeyboardParser.java
@@ -3,25 +3,25 @@ package mapping;
 public class KeyboardParser {
 	public KeyboardParser() {}
 	
-	//public static final int VK_LEFT = 37;
-	//public static final int VK_RIGHT = 227;
-	//public static final int VK_UP = 38;
-	//public static final int VK_DOWN = 40;
-	//public static final int SPACE = 32;
-	//public static final int ESCAPE = 27;
+	private static final int VK_LEFT = 37;
+	private static final int VK_RIGHT = 227;
+	private static final int VK_UP = 38;
+	private static final int VK_DOWN = 40;
+	private static final int SPACE = 32;
+	private static final int ESCAPE = 27;
 	
 	public void netParse(char c) {
-		if (c == (char) 37) {
+		if (c == (char) VK_LEFT) {
 			// goLeft
-		}else if (c == (char) 227){
+		}else if (c == (char) VK_RIGHT){
 			//goRight
-		}else if (c == (char) 38){
+		}else if (c == (char) VK_UP){
 			//goUp
-		}else if (c == (char) 40){
+		}else if (c == (char) VK_DOWN){
 			//goDown
-		}else if (c == (char) 32){
+		}else if (c == (char) SPACE){
 			//dropBomb
-		}else if (c == (char) 27){
+		}else if (c == (char) ESCAPE){
 			//esc
 		}else{
 			//doNothing
@@ -29,17 +29,17 @@ public class KeyboardParser {
 	}
 	
 	public void localParse(char c) {
-		if (c == (char) 37) {
+		if (c == (char) VK_LEFT) {
 			// goLeft
-		}else if (c == (char) 227){
+		}else if (c == (char) VK_RIGHT){
 			//goRight
-		}else if (c == (char) 38){
+		}else if (c == (char) VK_UP){
 			//goUp
-		}else if (c == (char) 40){
+		}else if (c == (char) VK_DOWN){
 			//goDown
-		}else if (c == (char) 32){
+		}else if (c == (char) SPACE){
 			//dropBomb
-		}else if (c == (char) 27){
+		}else if (c == (char) ESCAPE){
 			//esc
 		}else{
 			//doNothing
