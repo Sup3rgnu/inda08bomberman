1a3af846dcf939a30a3d5250b0b323d0147cbbba
caj.hofberg <caj.hofberg@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-03 13:14:32 +0000

    TODOs for popups

--- src/drawing/Popup.java
+++ src/drawing/Popup.java
@@ -7,4 +7,10 @@ abstract public class Popup {
 	static public void popupMessage(String s){
 		JOptionPane.showMessageDialog(null, s);
 	}
+	
+	//TODO: String popup for insertion of PlayerName
+	
+	//TODO: IP popup for insertion of IP addresses (unless String popup handles IPs)
+	
+	//TODO: Choose Server/Client popup
 }
