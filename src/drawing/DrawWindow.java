e161a9473eecee0df633edfb986eb12149ab6783
ealg3n <ealg3n@e68d3caa-1eba-11de-a78b-a7aca27b2395>
2009-04-20 16:00:22 +0000

    Tagit bort äckligt skräp
    Försökt lägga till en fungerande refresh funktion 
    
    Försökt undvika nullpointerexception.

--- src/drawing/DrawWindow.java
+++ src/drawing/DrawWindow.java
@@ -1,37 +1,44 @@
 package drawing;
 
 import java.awt.*;		// Includes many various classes
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 import java.util.Vector;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-import userInputHandling.KeyHandler;
-
-
+import userInputHandling.KeyHandler; 
 
 public class DrawWindow extends JFrame {
 	
-	public static DrawWindow dw = new DrawWindow();
-	
 	private static final long serialVersionUID = 7669473739626066784L;
 	
-	static final int WIDTH = 768;
-	static final int HEIGHT = 768;
+	static final int WIDTH = 16*32;
+	static final int HEIGHT = 16*32;
 	class Bob { int x, y, w, h, dx, dy; }
-	Canvas canvas; // Our drawing component
+	Canvas canvas = new Canvas(); // Our drawing component
+	BufferStrategy buffer;
+	BufferedImage bi;
+	Graphics grapichs;
+	public static DrawWindow dw;
+	
 	Vector< Point > circles = new Vector< Point >(); // Circles
 	Bob bob = new Bob(); // Our rectangle
-	Random rand = new Random(); // Used for random circle locations
+	
+	
+	
+    
+	
+	
+    
+	
+    Color background = Color.PINK;
 
 	
 
-	Graphics2D g2d = null;
-	Graphics graphics = null;
-	
-	
-	
 	public DrawWindow() {
 		
 		/* Start a key listener */
@@ -40,23 +47,110 @@ public class DrawWindow extends JFrame {
 		
     setIgnoreRepaint( true );
     setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    
+    canvas.setSize( WIDTH, HEIGHT );
     canvas = new Canvas();
     canvas.setIgnoreRepaint( true );
-    canvas.setSize( WIDTH, HEIGHT );
     add( canvas );
     pack();
     
+    canvas.createBufferStrategy( 2 );
+    buffer = canvas.getBufferStrategy();
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice gd = ge.getDefaultScreenDevice();
+    GraphicsConfiguration gc = gd.getDefaultConfiguration();
+    bi = gc.createCompatibleImage( WIDTH, HEIGHT );
     
     
+    bob.x = bob.y = 250;
+    bob.dx = bob.dy = 25;
+    bob.w = bob.h = 25;
     
- 
-}
     
+       
+	}  
 	
+	public void print(Graphics2D g2d) throws IOException{
+		Graphics graphics = null;
+	    
+	    
+		
+		try {
+		      
+	        // Clear the back buffer          
+	        //g2d = bi.createGraphics();
+	        //g2d.setColor( background );
+	        //g2d.fillRect( 0, 0, WIDTH, HEIGHT );
+	        
+	        // Draw help
+	        //g2d.setColor(  Color.BLACK );
+	        
+	        //g2d.drawString( "--------------------------", 250, 200 );
+	        //g2d.drawString( "New Game", 250, 210 );
+	        //g2d.drawString( "Bomberman.exe", 250, 220 );
+	        //g2d.drawString( "Press Q to disconnect", 250, 230 );
+	        //g2d.drawString( "--------------------------", 250, 240 );
+
+	        // Cajs tester
+	        //Image test = Toolkit.getDefaultToolkit().getImage("bombe/bombe0.gif");
+	         
+	        Image crate = ImageIO.read(new File("graphics/crate.gif"));
+	        Image stone = ImageIO.read(new File("graphics/stone.gif"));
+	        Image grass = ImageIO.read(new File("graphics/grass.gif"));
+	        Image player1 = ImageIO.read(new File("graphics/player1.gif"));
+	        Image player2 = ImageIO.read(new File("graphics/player2.gif"));
+	        		
+	        
+		        	
+			for(int i=0; i <mapping.Map.HEIGHT; i++){
+				for(int j=0; j < mapping.Map.WIDTH; j++){
+					if(mapping.Map.board[i][j] == mapping.Map.UNBREAKABLE){
+						g2d.drawImage(crate, i*32, j*32, 32, 32,this);
+					}
+					else if(mapping.Map.board[i][j] == mapping.Map.FREE){
+						g2d.drawImage(grass, i*32, j*32, 32, 32,this);
+					}
+					else if(mapping.Map.board[i][j] == mapping.Map.PLAYER1){
+						g2d.drawImage(player1, i*32, j*32, 32, 32,this);
+					}
+					else if(mapping.Map.board[i][j] == mapping.Map.PLAYER2){
+						g2d.drawImage(player2, i*32, j*32, 32, 32,this);
+					}
+					
+				}
+				System.out.println();
+			}
+			//g2d.drawImage(crate, 0, 0, 32, 32,this);
+	        
+	     
+	        // Blit image and flip...
+	        graphics = buffer.getDrawGraphics();
+	        graphics.drawImage( bi, 0, 0, null );
+	        if( !buffer.contentsLost() )
+	          buffer.show();
+	          
+	        // Let the OS have a little time...
+	        try {
+	          Thread.sleep(10);
+	        } catch (InterruptedException e) {
+	        }
+	        
+	      } finally {
+	        // Release resources
+	        if( graphics != null ) 
+	          graphics.dispose();
+	        if( g2d != null ) 
+	          g2d.dispose();
+	      	}
+	    }
+		
+	
+		
 	public static void DrawWindowmain() throws IOException {
-	
+	DrawWindow dw = new DrawWindow();
 	dw.setTitle( "Bomberman" );
 	dw.setVisible( true );
-	//dw.run();
+	
+    
   	}
 }
